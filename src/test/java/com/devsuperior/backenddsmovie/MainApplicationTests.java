105681f0468555fefef8c4d63eee8af752c20c28
Robson Sousa <robsoncartes@outlook.com>
2022-05-18 22:04:06 -0700

    Update 'pom.xml' file. Add to project new dependencies: 'junit-vintage-engine', 'mockito-core' and 'jacoco-maven-plugin'.

--- src/test/java/com/devsuperior/backenddsmovie/MainApplicationTests.java
+++ src/test/java/com/devsuperior/backenddsmovie/MainApplicationTests.java
@@ -1,9 +1,11 @@
 package com.devsuperior.backenddsmovie;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 class MainApplicationTests {
 
     @Test
