c8e639ba553492551dfcc713b2260ed42b88868e
Robson Sousa <robsoncartes@outlook.com>
2022-05-20 00:51:52 -0700

    'fix/class1': Update 'pom.xml' file. Switch Carriage Return (CRF) to Line Feed (LF) and resolving imports on 'MainApplicationTests' class.

--- src/test/java/com/devsuperior/backenddsmovie/MainApplicationTests.java
+++ src/test/java/com/devsuperior/backenddsmovie/MainApplicationTests.java
@@ -3,7 +3,6 @@ package com.devsuperior.backenddsmovie;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(MockitoJUnitRunner.class)
 class MainApplicationTests {
